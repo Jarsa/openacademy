d285d3c1310b6a712a7364b9d6f00841f67c5633
Alan Ramos <alan.ramos@jarsa.com.mx>
2016-08-16 16:18:11 -0700

    [REF] complete openerp.py file

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Openacademy",
+    "summary": "This module is to demostrate how to develop a module in Odoo",
+    "version": "9.0.1.0.0",
+    "category": "Development",
+    "website": "https://www.jarsa.com.mx",
+    "author": "Jarsa Sistemas",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "demo": [],
+}
